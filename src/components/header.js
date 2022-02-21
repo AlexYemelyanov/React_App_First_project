15e146545ca813b5040d4d2a438c690186263855
Alexey Emelyanov <a.emelyanov291189@gmail.com>
2022-02-21 09:46:39 +0300

    start project

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://www.tailorbrands.com/wp-content/uploads/2020/07/mcdonalds-logo.jpg"></img>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/profile.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://www.imgacademy.com/themes/custom/imgacademy/images/helpbox-contact.jpg"></img>
+      </div>
+      <div>avatar+description</div>
+      <div>
+        My posts
+        <div>New posts</div>
+        <div>Post 1</div>
+        <div>Post 2</div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
